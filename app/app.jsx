a8fc0517836677f59632da1e30c1d02e6c707b86
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-01-21 19:30:20 -0500

    Created routes for every page

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hasHistory} = require('react-router');
-
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+var Contact = require('Contact');
 
 ReactDOM.render(
-    <h1>Boilerplate app!</h1>,
+    <Router history={hashHistory}>
+        <Route path='/' component={Main}>
+            <Route path='about' component={About}/>
+            <Route path='contact' component={Contact}/>
+            <IndexRoute component={Weather}/>
+        </Route>
+    </Router>,
     document.getElementById('app')
 );
---
+++ app/components/Contact.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Contact = React.createClass({
+    render: function () {
+        return (
+            <h3>Contact</h3>
+        );
+    }
+});
+
+module.exports = Contact;
